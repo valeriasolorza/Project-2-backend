b47e6cca932f5d856f7de03534755136c3e0f77c
valeriasolorza <v.solorza.343@gmail.comcd>
2024-07-17 20:38:18 -0700

    Random meal route fixed

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const {
-  getAllRecipes,
-  createRecipe,
-  getRecipeById,
-  updateRecipe,
-  deleteRecipe
-} = require('../controllers/recipeController');
 
-router.get('/', getAllRecipes);
-router.post('/', createRecipe);
-router.get('/:id', getRecipeById);
-router.put('/:id', updateRecipe);
-router.delete('/:id', deleteRecipe);
+// const {
+//   getAllRecipes,
+//   createRecipe,
+//   getRecipeById,
+//   updateRecipe,
+//   deleteRecipe
+// } = require('../controllers/recipeController');
+
+// router.get('/', getAllRecipes);
+// router.post('/', createRecipe);
+// router.get('/:id', getRecipeById);
+// router.put('/:id', updateRecipe);
+// router.delete('/:id', deleteRecipe);
 
 // Endpoint to fetch a random meal from external API
-router.get('/random-meal', async (req, res) => {
+router.get("/random-meal", async (req, res) => {
   try {
     // Make a request to TheMealDB API for a random meal
     const response = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
-    
+
     // Send the API response to the client
     res.json(response.data);
   } catch (error) {
