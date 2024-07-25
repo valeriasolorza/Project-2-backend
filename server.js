b932d88d0d5affe71aeb39f06e3d9aeef5200bdd
Valeria <150319227+valeriasolorza@users.noreply.github.com>
2024-07-24 20:00:13 -0700

    railway

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const app = require('./app');
 const sequelize = require('./config/db');
 const router = require('./routes/recipeRoutes.js')
 require('dotenv').config();
+const path = require("path");
+const express = require('express')
 
 const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname, 'public', 'build')));
+  }
+
 // Start the server after ensuring database connection
 sequelize.authenticate()
   .then(() => {
